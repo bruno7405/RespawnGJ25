34dcb727658166cfa8a68ab1dbd0c9957e3e171c
Bruno Wu <bruno7405@gmail.com>
2025-10-10 21:26:24 -0500

    Adins stuff

---
+++ ProjectDayNight/Assets/Employee/Employee.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Employee : MonoBehaviour
+{
+    private const int wallLayer = 1 << 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void 
+}
