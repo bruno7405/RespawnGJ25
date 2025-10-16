97845858e7919ffd746d408f6c72e719145fd9bc
Bruno Wu <bruno7405@gmail.com>
2025-10-16 18:18:52 -0500

    Lives UI

--- ProjectDayNight/Assets/UI/PlayerStatsUI/PlayerStatsUI.cs
+++ ProjectDayNight/Assets/UI/PlayerStatsUI/PlayerStatsUI.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatsUI : MonoBehaviour
 {
     private static PlayerStatsUI instance;
     public static PlayerStatsUI Instance => instance;
 
+    [SerializeField] List<GameObject> blobs = new List<GameObject>();
     [SerializeField] TextMeshProUGUI moneyTMP;
 
 
@@ -16,10 +19,28 @@ public class PlayerStatsUI : MonoBehaviour
             Destroy(this);
         }
         instance = this;
+
+        SetLives(2);
     }
 
     public void SetMoney(int money)
     {
         moneyTMP.text = "$ " + money.ToString();
+    }
+    
+    public void SetLives(int lives)
+    {
+        if (lives > 3) Debug.LogError("Can't set lives more than 3");
+        for (int i = 0; i < blobs.Count; i++)
+        {
+            if (i < lives)
+            {
+                blobs[i].GetComponent<Image>().enabled = true;
+            }
+            else
+            {
+                blobs[i].GetComponent<Image>().enabled = false;
+            }
+        }
     }
 }
